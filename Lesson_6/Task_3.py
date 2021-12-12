74d321ebaea904762d91094f8ca99331ce5844f2
Andrei <werdender55@gmail.com>
2021-12-12 21:49:17 +0200

    .

--- Lesson_6/Task_3.py
+++ Lesson_6/Task_3.py
@@ -1,17 +1,2 @@
-import random
-a_list = [random.randrange(1, 20, 1) for i in
-          range(random.randrange(2, 5, 1))]
-b_list = [random.randrange(1, 20, 1) for j in
-          range(random.randrange(2, 5, 1))]
-# print(a_list)
-# print(b_list)
-c=a_list+b_list
-n=0
-for d in c:
-    if d in c[d+1::]:
-        continue
-else:
-    n+=1
-print(c)
-print(n)
+
 
