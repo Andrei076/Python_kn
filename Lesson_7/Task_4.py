5c8d686034587379bafc5764b2d5b8e78dd9ee69
Andrei <werdender55@gmail.com>
2021-12-14 18:11:52 +0200

    .

--- Lesson_7/Task_4.py
+++ Lesson_7/Task_4.py
@@ -1,1 +1,13 @@
-
+text = "Кто же вас гонит: судьбы ли решение?Зависть ли тайная? злоба ль " \
+       "открытая?Или на вас тяготит преступление?Или друзей клевета ли " \
+       "ядовитая?"
+d = dict()
+for i in text.split():
+    if d.get(i) is not None:
+        d[i] += 1
+    else:
+        d[i] = 1
+m = max(d.values())
+for key in sorted(d.keys()):
+    if d[key] == m:
+        print(key)
