8f2527731923645a8ebf5d56aa62876f2aae9573
Andrei <werdender55@gmail.com>
2021-12-14 18:30:00 +0200

    .

--- Lesson_7/Task_4.py
+++ Lesson_7/Task_4.py
@@ -1,14 +1,15 @@
 text = "Кто же вас гонит: судьбы решение? Зависть ли тайная? злоба ль " \
-       "открытая? Или на вас тяготит преступление? Или друзей клевета " \
+       "открытая? Или на вас тяготит преступление? Или вас Или друзей " \
+       "клевета " \
        "ядовитая?"
 d = dict()
-for i in text.split():
+for i in reversed(text.split()):
     if d.get(i) is not None:
         d[i] += 1
     else:
         d[i] = 1
 m = max(d.values())
-for key in sorted(d.keys()):
+for key in d.keys():
     if d[key] == m:
         print(key)
         break
