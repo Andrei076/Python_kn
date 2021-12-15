eb9c3cc03424b69b20ca6415fd61008f9f671032
Andrei <werdender55@gmail.com>
2021-12-15 14:19:21 +0200

    .

--- Lesson_8/Part_1/Task_2.py
+++ Lesson_8/Part_1/Task_2.py
@@ -3,6 +3,4 @@ a_list = [random.randrange(1, 50, 1) for i in
           range(random.randrange(2, 20, 1))]
 b_list = [random.randrange(1, 50, 1) for j in
           range(random.randrange(2, 20, 1))]
-print(a_list)
-print(b_list)
 print(len(set(b_list) & set(a_list)))
